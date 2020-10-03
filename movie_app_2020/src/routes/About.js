831a12575694251a5bd004f6c4764df0f8512801
unknown <gustn1104@gmail.com>
2020-10-04 05:32:04 +0900

    최종 완성본

---
+++ movie_app_2020/src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                Made by Hyunsoo Park
+            </span>
+            <span>-2020 September-</span>
+        </div>
+    );
+}
+
+export default About;
